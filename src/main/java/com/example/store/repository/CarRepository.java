3cef3287a8bebc70e029984b9bf12d5b093e063c
vlad85110 <92858792+vlad85110@users.noreply.github.com>
2023-09-24 13:29:10 +0700

    Изменил запрос и теперь оно запускается (теперь есть джоин и подзапрос)

--- src/main/java/com/example/store/repository/CarRepository.java
+++ src/main/java/com/example/store/repository/CarRepository.java
@@ -6,11 +6,13 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CarRepository extends JpaRepository<Car, Long> {
-    @Query("select car_id, customer_id from orders join cars_orders_products" +
-            " on orders.id = cars_orders_products.order_id" +
-            " join products on cars_orders_products.product_id = products.id" +
-            " where product_id = :pr_id and orders.is_open = true")
+    @Query("select distinct c from Car c where c.id in" +
+            " (select cop.car.id from Order o join CarOrderProduct cop " +
+            " on o.id = cop.order.id join Product p " +
+            "on cop.product.id = p.id " +
+            "where p.id = :pr_id and o.isOpen = true)")
     List<Car> findCarByProductInWork(@Param("pr_id") Long productId);
 }

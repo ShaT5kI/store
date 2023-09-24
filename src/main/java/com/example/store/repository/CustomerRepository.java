d037149fbff5127eef7448daec4efe2b6c65865d
vlad85110 <92858792+vlad85110@users.noreply.github.com>
2023-09-24 23:09:33 +0700

    Добавил запрос в CustomerRepository c group by и having

--- src/main/java/com/example/store/repository/CustomerRepository.java
+++ src/main/java/com/example/store/repository/CustomerRepository.java
@@ -2,8 +2,18 @@ package com.example.store.repository;
 
 import com.example.store.entity.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    @Query("select customer from Customer customer where customer.id in" +
+            " (select c.id from Customer c join Order o on c.id = o.customer.id" +
+            " join CarOrderProduct cop on o.id = cop.order.id" +
+            " join Product p on cop.product.id = p.id" +
+            " group by c.id having sum(p.price) > :pr)")
+    List<Customer> findByProductSum(@Param("pr") Integer price);
 }

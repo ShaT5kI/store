69c8d2726f32cb3ae01c03e414a67d2f6d21574b
ShaT5kI <i.shatskikh@g.nsu.ru>
2023-10-02 07:19:53 +0700

    Добавил кортежи в flyway

--- src/main/resources/db/migration/V1__create_table.sql
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -71,3 +71,34 @@ create sequence cars_seq
 create sequence cars_orders_products_seq
     increment by 50;
 
+
+
+INSERT INTO persons (id, name)
+VALUES (1, 'Person 1'),
+       (2, 'Person 2'),
+       (3, 'Person 3'),
+       (4, 'Person 4');
+
+INSERT INTO suppliers (documents_package, guarantee, name)
+VALUES (true, false, 'Supplier A'),
+       (false, true, 'Supplier B');
+
+INSERT INTO owners (id, supplier_id)
+VALUES (1, 1),
+       (2, 2);
+
+INSERT INTO customers (id)
+VALUES (3),
+       (4);
+
+INSERT INTO products (id, name, price, supplier_id)
+VALUES (1, 'Product A', 100, 1),
+       (2, 'Product B', 150, 2);
+
+INSERT INTO cars (id, name, customer_id)
+VALUES (1, 'Car X', 3),
+       (2, 'Car Y', 4);
+
+INSERT INTO orders (id, is_open, customer_id, supplier_id)
+VALUES (1, true, 3, 1),
+       (2, false, 4, 2);
